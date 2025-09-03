b64294b57863844adde4dff7be1e874b894f9449
TanvirTian <beachpilis@gmail.com>
2025-09-03 18:25:59 +0600

    feat: Update CORS function in main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -138,9 +138,14 @@ func writeJSON(w http.ResponseWriter, code int, v interface{}) {
 
 func withCORS(next http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Access-Control-Allow-Origin", "*")
+        origin := r.Header.Get("Origin") 
+        if origin != "" {
+            w.Header().Set("Access-Control-Allow-Origin", origin)
+            w.Header().Set("Vary", "Origin")
+        }
         w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
         w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Credentials", "true") 
 
         if r.Method == http.MethodOptions {
             w.WriteHeader(http.StatusNoContent)
@@ -149,6 +154,7 @@ func withCORS(next http.Handler) http.Handler {
         next.ServeHTTP(w, r)
     })
 }
+
 
 func main() {
     s := newServer()
